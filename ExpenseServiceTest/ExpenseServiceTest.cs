39396aef16533e1efc3aaa8d67279a8e863ad0f8
RZhang16 <RZhang16@slb.com>
2022-10-26 13:46:18 +0800

    test: Add three test

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -22,32 +22,44 @@ namespace Expense.Service.Test
         public void Should_return_expense_type_A_if_project_is_external_and_name_is_project_A()
         {
             // given
+            Project project = new Project(ProjectType.EXTERNAL, "Project A");
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
             // then
+            Assert.Equal(ExpenseType.EXPENSE_TYPE_A, expenseType);
         }
 
         [Fact]
         public void Should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
         {
             // given
+            Project project = new Project(ProjectType.EXTERNAL, "Project B");
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
             // then
+            Assert.Equal(ExpenseType.EXPENSE_TYPE_B, expenseType);
         }
 
         [Fact]
         public void Should_return_other_expense_type_if_project_is_external_and_has_other_name()
         {
             // given
+            Project project = new Project(ProjectType.EXTERNAL, "other name");
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
             // then
+            Assert.Equal(ExpenseType.OTHER_EXPENSE, expenseType);
         }
 
         [Fact]
         public void Should_throw_unexpected_project_exception_if_project_is_invalid()
         {
             // given
+            Project project = new Project(ProjectType.EXTERNAL, "Project B");
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
             // then
+            Assert.Equal(ExpenseType.EXPENSE_TYPE_B, expenseType);
         }
     }
 }
